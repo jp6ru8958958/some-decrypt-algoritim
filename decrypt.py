525590052b691842080ce96a082b30992ee28a0f
jp6ru8958958 <jp6ru8958958@gmail.com>
2020-11-16 12:18:55 +0800

    discription update

--- decrypt.py
+++ decrypt.py
@@ -27,7 +27,11 @@ def A1Z26(cipherText):
     for text in cipherText:
         split_cipherText_by_Dash = text.split('-')
         for mainText in split_cipherText_by_Dash:
-            print(chr(int(mainText)+ord('A')-1), end='')
+            try:
+                print(chr(int(mainText)+ord('A')-1), end='')
+            except ValueError:
+                print(mainText, end='')
+
         print(' ', end='')
     return
 
@@ -46,7 +50,7 @@ def vigenere(cipherText, key):
 
 if __name__ == '__main__':
     decrypt_way = sys.argv[1]
-    cipherText = input('Plaintext:')
+    cipherText = input('Ciphertext: ')
     if decrypt_way == 'caesar':
         caesar(cipherText)
     elif decrypt_way == 'atbash':
@@ -56,3 +60,23 @@ if __name__ == '__main__':
     elif decrypt_way == 'vigenere':
         key = input('KEY:')
         vigenere(cipherText, key)
+
+
+'''
+caesar:
+ZHOFRPH WR JUDYLWB IDOOV
+
+atbash:
+HLIIB, WRKKVI, YFG BLFI DVMWB RH RM ZMLGSVI XZHGOV.
+
+a1z26:
+2-21-20 23-8-15 19-20-15-12-5 20-8-5  3-1-16-5-18-19?
+
+vigenere:
+MXNGVEECW MW SLAWW. SUL FPZSK MW SOJMRX.
+ERASE
+
+a1z26 -> atbash -> caesar:
+4-16-19 11-23-10 20-9-1-10-5-4-23-15-6-5 15-5 2-19-6-25 21-12-19-2-19-6
+21-23-10 16-19 16-15-20-19 16-15-5 8-12-23-10-5 18-9-6-19-2-19-6 ?
+'''
